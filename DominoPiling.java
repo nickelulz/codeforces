f8ef20990135d7d00f6f50344e2785c8445546ee
nickel <sandmanmufmaf@gmail.com>
2021-11-24 11:03:17 -0600

    Create DominoPiling.java

---
+++ DominoPiling.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println((input.nextInt() * input.nextInt()) / 2);
+    }
+}
