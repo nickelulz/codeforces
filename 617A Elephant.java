ce891531d5399b9a9dcc8be3373c5040d7a986c9
nickel <sandmanmufmaf@gmail.com>
2021-12-06 17:54:08 -0600

    Create 617A Elephant.java

---
+++ 617A Elephant.java
@@ -0,0 +1,9 @@
+    import java.util.*;
+    import java.io.*;
+     
+    public class Main {
+    	public static void main(String[] args) {
+    		Scanner input = new Scanner(System.in);
+    		int x = input.nextInt(); System.out.println(x%5==0?x/5:x/5+1);
+    	}
+    }
